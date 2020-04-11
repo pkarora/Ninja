f688853c434d5b5c96e7cf027d9c402af37e974d
pkarora <pkrocks127@gmail.com>
2020-04-11 16:36:14 +0530

    reverse

---
+++ Reverse of a number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+ // Write a program to generate the reverse of a given number N.
+{
+int n;
+cin>>n;
+int reverse = 0;
+int i=n
+while (i>=0)
+ {
+  /* code */
+  int k=i%10;
+  reverse = reverse + k*10;
+  i = i/10;
+ }
+ cout<<reverse;
+}
