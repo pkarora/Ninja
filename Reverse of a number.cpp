d724486e591b25562fa58687a732427b2d1b3ff9
pkarora <63202147+pkarora@users.noreply.github.com>
2020-04-11 23:58:32 +0530

    reverse complete

--- Reverse of a number.cpp
+++ Reverse of a number.cpp
@@ -6,12 +6,12 @@ int main()
 int n;
 cin>>n;
 int reverse = 0;
-int i=n
-while (i>=0)
+int i=n;
+while (i>0)
  {
   /* code */
   int k=i%10;
-  reverse = reverse + k*10;
+  reverse = reverse*10 + k;
   i = i/10;
  }
  cout<<reverse;
